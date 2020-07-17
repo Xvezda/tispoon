24b93b2724ff79d3321ad4357e1b4f886ad97fd9
Xvezda <xvezda@naver.com>
2020-07-18 03:35:27 +0900

    Update test_core.py

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,9 +6,16 @@ import pytest
 from tispoon import core
 
 
+@pytest.fixture
+def tispoon_cli():
+    return core.Tispoon()
+
+
 def test_dotget():
     fake = {"foo": {"bar": "baz"}}
     assert core.dotget(fake, "foo.bar") == "baz"
+    assert core.dotget(fake, "foo.egg") == None
+    assert core.dotget(fake, "egg.spam") == None
 
 
 if __name__ == "__main__":
